eb9b968f965db86b4a451eb5acf179f2a5c469b3
Zabihulla Shaik <informshaikz@gmail.com>
2023-07-31 12:15:46 +0530

    feat: geometry, material, mesh, animation loop

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
 renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus);
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
